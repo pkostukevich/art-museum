a266d6d1fcc93069aa567a8e4d18ea0618bce79c
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-10-30 23:54:02 +0300

    fix: add eslint rules and fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,38 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error", 
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+          { argsIgnorePattern: "^_" }, 
+        ],
+      "@typescript-eslint/typedef": [
+        "error",
+          {
+            parameter: true, 
+            propertyDeclaration: true,
+            variableDeclaration: true,
+            memberVariableDeclaration: true,
+            variableDeclarationIgnoreFunction: true,
+          },
+        ],
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  }
 ];
