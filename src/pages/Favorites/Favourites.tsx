abf5e2de26920ff554fa6f34cca593c1f750c376
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-10-31 19:35:47 +0300

    feat: implement routing

---
+++ src/pages/Favorites/Favourites.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Favourites: React.FC = () => {
+  return (
+    <div>
+      <h1>Favourites</h1>
+    </div>
+  );
+};
---
+++ src/pages/Painting/Painting.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Painting: React.FC = () => {
+  return (
+    <div>
+      <h1>Painting</h1>
+    </div>
+  );
+};
