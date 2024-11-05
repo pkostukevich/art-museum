9f8f7124eb8bad9ea13f05da945626fabccfecf2
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-11-05 17:47:32 +0300

    fix: remove App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from '@pages/Home/Home';
+import Favorites from '@pages/Favorites/Favorites';
+import Painting from '@pages/PaintingInfo/PaintingInfo';
+import Layout from '@components/Layout/Layout';
+import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary';
+import '@styles/common.scss';
 
 const root: ReactDOM.Root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/paintings/:id" element={<Painting />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </ErrorBoundary>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>,
 );
