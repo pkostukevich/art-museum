f1903a305a6137f0cb936cb72eea77fbfbfbfceb
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-11-05 17:08:49 +0300

    feat: implement search form logic

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,27 +1,59 @@
 import React, { useEffect } from 'react';
 import { useDebounce } from '@hooks/useDebounce';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 import './SearchForm.scss';
 
-const SearchForm: React.FC = () => {
-  const { register, watch } = useForm();
-  const searchTerm: string = watch('search');
-  const debouncedSearchTerm: string = useDebounce(searchTerm, 500);
+type SearchFormInputs = {
+  search?: string;
+};
+
+type SearchFormProps = {
+  onSearch: (term: string) => void;
+};
+
+const schema: Yup.ObjectSchema<
+  SearchFormInputs,
+  Yup.AnyObject,
+  Yup.AnyObject,
+  ''
+> = Yup.object().shape({
+  search: Yup.string(),
+});
+
+const SearchForm: React.FC<SearchFormProps> = ({ onSearch }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<SearchFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const searchTerm: string | undefined = watch('search');
+  const debouncedSearchTerm: string | undefined = useDebounce(searchTerm, 300);
 
   useEffect(() => {
-    if (debouncedSearchTerm) {
-      console.log(debouncedSearchTerm);
+    if (debouncedSearchTerm !== undefined) {
+      onSearch(debouncedSearchTerm || '');
     }
-  }, [debouncedSearchTerm]);
+  }, [debouncedSearchTerm, onSearch]);
+
+  const onSubmit: SubmitHandler<SearchFormInputs> = (data) => {
+    onSearch(data.search || '');
+  };
 
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit(onSubmit)}>
       <input
         className="search-form__input"
         type="text"
         {...register('search')}
         placeholder="Search Art, Artist, Work..."
       />
+      {errors.search && <p className="error">{errors.search.message}</p>}
     </form>
   );
 };
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 
-export function useDebounce(value: string, delay: number): string {
-  const [debouncedValue, setDebouncedValue] = useState<string>(value);
+export function useDebounce(
+  value: string | undefined,
+  delay: number,
+): string | undefined {
+  const [debouncedValue, setDebouncedValue] = useState<string | undefined>(
+    value,
+  );
 
   useEffect(() => {
     const handler: NodeJS.Timeout = setTimeout(() => {
