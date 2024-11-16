1ba6051333e67e84ee1bb32ea79ce1b635a5702f
Polina Kostukevich <kostukevich.p9@gmail.com>
2024-11-16 15:07:55 +0300

    feat: implement outside-click hook

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { Location, useLocation } from 'react-router-dom';
 
+import NavItem from '@components/ui/NavItem';
 import { ROUTES } from '@constants/routes';
+import useClickOutside from '@hooks/useClickOutside';
 import { NavItemInterface } from '@models/interfaces/navItem.interface';
 import MuseumLogo from '@svg/museum-logo-light.svg';
 import Favorites from '@svg/navigation/favorites.svg';
 import Home from '@svg/navigation/home.svg';
 
-import NavItem from '../ui/NavItem';
 import {
   BurgerLine,
   BurgerMenu,
@@ -31,26 +33,32 @@ const navItems: NavItemInterface[] = [
 
 const Header: React.FC = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const menuRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+  const location: Location = useLocation();
 
   const toggleMenu: () => void = useCallback(() => {
     setMenuOpen((prev) => !prev);
   }, []);
 
+  useClickOutside(menuRef, () => setMenuOpen(false));
+
   return (
     <HeaderContainer>
       <HeaderWrapper>
         <Logo src={MuseumLogo} alt="Museum of Art logo" />
-        <div>
+        <div ref={menuRef}>
           <Nav isOpen={isMenuOpen}>
-            {navItems.map(({ path, icon, label }, index) => (
-              <NavItem
-                key={index}
-                path={path}
-                icon={icon}
-                label={label}
-                handleClick={toggleMenu}
-              />
-            ))}
+            {navItems
+              .filter((navItem) => navItem.path !== location.pathname)
+              .map(({ path, icon, label }, index) => (
+                <NavItem
+                  key={index}
+                  path={path}
+                  icon={icon}
+                  label={label}
+                  handleClick={toggleMenu}
+                />
+              ))}
           </Nav>
           <BurgerMenu onClick={toggleMenu} aria-expanded={isMenuOpen}>
             <BurgerLine isOpen={isMenuOpen} />
